00ca11670c4850f6e20986b0e4deeb9db5995bb1
rdeyvil <rdeyvil11@gmail.com>
2019-11-04 14:59:05 +0545

    Sets up User authentication using Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'post/new'
+  get 'post/create'
+  get 'post/index'
+  get 'post/edit'
+  get 'post/update'
+  get 'post/destroy'
+  get 'post/new'
+  get 'post/create'
+ 
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'post#index'
+
+   
 end
