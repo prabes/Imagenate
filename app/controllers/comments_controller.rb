5205beb5b7b2f54b27d3ea3f1ac8efeeecdc9c05
rdeyvil <rdeyvil11@gmail.com>
2019-11-11 00:23:50 +0545

    Adds postcard for everypost

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: %i[show edit update destroy]
@@ -13,43 +15,42 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @user = current_user
-    @post = 
-    if comment.save
-      flash[:sucess] = 'Successfully created!'
-      redirect_to root_path
-    else
-      flash[:notice] = 'Something Went wrong!'
-      render 'new'
-    end
+    @post =
+      if comment.save
+        flash[:sucess] = 'Successfully created!'
+        redirect_to root_path
+      else
+        flash[:notice] = 'Something Went wrong!'
+        render 'new'
+      end
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @comment.update(comment_params)
-      flash[:notice] = "Successfully updated!"
+      flash[:notice] = 'Successfully updated!'
       redirect_to post_path
     else
-      flash[:alert] = "Failed to Update!"
+      flash[:alert] = 'Failed to Update!'
     end
   end
 
   def destroy
-    @comment.destroy if @comment
-    flash[:notice] = "Successfully Deleted!"
+    @comment&.destroy
+    flash[:notice] = 'Successfully Deleted!'
     redirect_to post_path
   end
 
   private
+
   def set_comment
     @comment = Comment.find(params[:id])
   end
-  
+
   def comment_params
     permit.require(:comment).permit(:body)
-  end 
+  end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class Profile < ApplicationRecord
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
-  
+  has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }
+
   # Validate content type
   validates_attachment_content_type :avatar, content_type: /\Aimage/
   # Validate filename
   validates_attachment_file_name :avatar, matches: [/png\z/, /jpe?g\z/]
-  
-  belongs_to :user 
+
+  belongs_to :user
 end
