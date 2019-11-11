acd0498626bb998f3ed69377692958a911512db3
rdeyvil <rdeyvil11@gmail.com>
2019-11-11 23:17:49 +0545

    uploads multiple Images-polymorphic associations

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,13 @@
 class Image < ApplicationRecord
-   has_attached_file :image
+   has_attached_file :image, styles: {small: "200x200", medium: "700x700>", thumb: "100x100>" }
 
   validates_attachment :image, content_type: { content_type:
                         ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'] }
    belongs_to :imageable, polymorphic: true 
+
+   # def url(thumb)  
+   #    image.url(:thumb)
+   # end
+
+
 end
