bb6fe5cab73e9060d2014e97472a238c0eb4bd89
rdeyvil <rdeyvil11@gmail.com>
2019-11-11 22:43:02 +0545

    Adds polymorphic associations

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,9 @@
+class ImagesController < ApplicationController
+  def index
+    @image = Image.all
+  end
+
+  def new 
+    @image = Image.new 
+  end
+end
---
+++ app/models/image.rb
@@ -0,0 +1,7 @@
+class Image < ApplicationRecord
+   has_attached_file :image
+
+  validates_attachment :image, content_type: { content_type:
+                        ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'] }
+   belongs_to :imageable, polymorphic: true 
+end
