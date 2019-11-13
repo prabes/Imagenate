9441af05158b0e7f745d44314c86612462c3f0b4
rdeyvil <rdeyvil11@gmail.com>
2019-11-13 11:09:25 +0545

    Creates and shows comments for a post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
+  validates :body, presence: true
   belongs_to :user
-  belongs_to :post
+  belongs_to :post, dependent: :destroy
 end
