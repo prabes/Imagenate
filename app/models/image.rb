fddebf2669ce0b2cb5d4bdddac67f4da87e81f9c
rdeyvil <rdeyvil11@gmail.com>
2019-11-26 16:23:18 +0545

    Improvize user-interaction div

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,13 +1,7 @@
 class Image < ApplicationRecord
    has_attached_file :image, styles: {small: "200x200", medium: "700>", thumb: "100x100>" }
 
-  validates_attachment :image, content_type: { content_type:
+   validates_attachment :image, content_type: { content_type:
                         ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'] }
    belongs_to :imageable, polymorphic: true 
-
-   # def url(thumb)  
-   #    image.url(:thumb)
-   # end
-
-
 end
